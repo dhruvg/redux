05093e5574f604858beb5f9760549347afb3b5f6
Dhruv Garg <dhruv.garg@gmail.com>
2016-06-25 21:17:45 -0400

    Setup react-hot-loader with todos example.

--- examples/todos/server.js
+++ examples/todos/server.js
@@ -1,18 +1,19 @@
 var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
+var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config')
 
-var app = new (require('express'))()
+var app = new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+})
 var port = 3000
 
 var compiler = webpack(config)
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
 app.use(webpackHotMiddleware(compiler))
-
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/index.html')
-})
 
 app.listen(port, function(error) {
   if (error) {
--- examples/todos/webpack.config.js
+++ examples/todos/webpack.config.js
@@ -4,6 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
     'webpack-hot-middleware/client',
     './index'
   ],
@@ -20,7 +22,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loaders: [ 'babel' ],
+        loaders: ['react-hot', 'babel'],
         exclude: /node_modules/,
         include: __dirname
       }
